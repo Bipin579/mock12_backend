09e3947b4498c1b336bbe3f790283d9c3f6b2752
Bipin <biping579@gmail.com>
2023-04-04 13:44:45 +0530

    backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connection = require('./config/db');
 const userRoute = require('./routes/userRoute');
+const calcRoute = require('./routes/calculateRoute');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
+app.use(calcRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
